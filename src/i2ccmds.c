461f8b44b63aaaafea8c5cd9335bb7d90c540ed6
Kevin O'Connor <kevin@koconnor.net>
2023-06-08 11:20:59 -0400

    Kconfig: Support selecting optional features on chips with small flash size
    Add a new HAVE_LIMITED_CODE_SIZE symbol that enables a menu to select
    optional features.  This symbol is enabled on chips with small build
    sizes.
    
    Replace the HAVE_GPIO_BITBANGING with four new symbols:
    WANT_GPIO_BITBANGING, WANT_DISPLAYS, WANT_SENSORS, WANT_SOFTWARE_SPI,
    and WANT_SOFTWARE_I2C.  This allows users a little more flexibility
    when selecting features they need.
    
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- src/i2ccmds.c
+++ src/i2ccmds.c
@@ -5,6 +5,7 @@
 // This file may be distributed under the terms of the GNU GPLv3 license.
 
 #include <string.h> // memcpy
+#include "autoconf.h" // CONFIG_WANT_SOFTWARE_I2C
 #include "basecmd.h" //oid_alloc
 #include "command.h"  //sendf
 #include "sched.h" //DECL_COMMAND
@@ -35,7 +36,11 @@ command_i2c_write(uint32_t *args)
     struct i2cdev_s *i2c = oid_lookup(oid, command_config_i2c);
     uint8_t data_len = args[1];
     uint8_t *data = command_decode_ptr(args[2]);
-    i2c_write(i2c->i2c_config, data_len, data);
+    uint_fast8_t flags = i2c->flags;
+    if (CONFIG_WANT_SOFTWARE_I2C && flags & IF_SOFTWARE)
+        i2c_software_write(i2c->i2c_software, data_len, data);
+    else
+        i2c_write(i2c->i2c_config, data_len, data);
 }
 DECL_COMMAND(command_i2c_write, "i2c_write oid=%c data=%*s");
 
@@ -48,7 +53,11 @@ command_i2c_read(uint32_t * args)
     uint8_t *reg = command_decode_ptr(args[2]);
     uint8_t data_len = args[3];
     uint8_t data[data_len];
-    i2c_read(i2c->i2c_config, reg_len, reg, data_len, data);
+    uint_fast8_t flags = i2c->flags;
+    if (CONFIG_WANT_SOFTWARE_I2C && flags & IF_SOFTWARE)
+        i2c_software_read(i2c->i2c_software, reg_len, reg, data_len, data);
+    else
+        i2c_read(i2c->i2c_config, reg_len, reg, data_len, data);
     sendf("i2c_read_response oid=%c response=%*s", oid, data_len, data);
 }
 DECL_COMMAND(command_i2c_read, "i2c_read oid=%c reg=%*s read_len=%u");
@@ -67,12 +76,20 @@ command_i2c_modify_bits(uint32_t *args)
     uint8_t *clear_set = command_decode_ptr(args[4]);
     uint8_t receive_data[reg_len + data_len];
     memcpy(receive_data, reg, reg_len);
-    i2c_read(i2c->i2c_config, reg_len, reg, data_len, receive_data + reg_len);
+    if (CONFIG_WANT_SOFTWARE_I2C && flags & IF_SOFTWARE)
+        i2c_software_read(
+            i2c->i2c_software, reg_len, reg, data_len, receive_data + reg_len);
+    else
+        i2c_read(
+            i2c->i2c_config, reg_len, reg, data_len, receive_data + reg_len);
     for (int i = 0; i < data_len; i++) {
         receive_data[reg_len + i] &= ~clear_set[i];
         receive_data[reg_len + i] |= clear_set[data_len + i];
     }
-    i2c_write(i2c->i2c_config, reg_len + data_len, receive_data);
+    if (CONFIG_WANT_SOFTWARE_I2C && flags & IF_SOFTWARE)
+        i2c_software_write(i2c->i2c_software, reg_len + data_len, receive_data);
+    else
+        i2c_write(i2c->i2c_config, reg_len + data_len, receive_data);
 }
 DECL_COMMAND(command_i2c_modify_bits,
              "i2c_modify_bits oid=%c reg=%*s clear_set_bits=%*s");
